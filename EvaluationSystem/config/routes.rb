b40742d96651ae9801304bc444b80d8d9f3d9bf6
unknown <travisle22@gmail.com>
2019-11-01 15:47:58 -0700

    Added the ability to put a file on the browser Added a small function to at least get the file

--- EvaluationSystem/config/routes.rb
+++ EvaluationSystem/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'uploader/proUpload'
+  post 'uploader/proUpload', to: 'uploader#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
