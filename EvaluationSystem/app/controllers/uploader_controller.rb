80eda1dcd4b88c936045353f8827dc2433af6575
unknown <travisle22@gmail.com>
2019-11-01 15:47:58 -0700

    Added the ability to put a file on the browser Added a small function to at least get the file

--- EvaluationSystem/app/controllers/uploader_controller.rb
+++ EvaluationSystem/app/controllers/uploader_controller.rb
@@ -1,4 +1,12 @@
 class UploaderController < ApplicationController
-  def proUpload
+  def new
+    @file_n = Uploader.new
+  end
+  def upload
+    uploaded_file = params[:file]
+    puts 1
+  end
+  def extension_white_list
+    %w(csv)
   end
 end
