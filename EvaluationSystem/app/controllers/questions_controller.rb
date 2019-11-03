d3ea61b4f09a46935d7f328886944bb7d43c1f51
Emily Zhang <emily@Emilys-MacBook-Pro.local>
2019-11-03 01:24:19 -0800

    Added export functionality - not tested

--- EvaluationSystem/app/controllers/questions_controller.rb
+++ EvaluationSystem/app/controllers/questions_controller.rb
@@ -10,6 +10,7 @@ class QuestionsController < ApplicationController
   # GET /questions/1
   # GET /questions/1.json
   def show
+    @question = Question.find(params[:question_ID])
   end
 
   # GET /questions/new
@@ -19,6 +20,7 @@ class QuestionsController < ApplicationController
 
   # GET /questions/1/edit
   def edit
+    @question = Question.find(params[:question_ID])
   end
 
   # POST /questions
--- EvaluationSystem/app/models/survey.rb
+++ EvaluationSystem/app/models/survey.rb
@@ -1,2 +1,13 @@
 class Survey < ApplicationRecord
+
+    def self.to_csv
+        attributes = %w{Q1 Q2 Q3 Q4 Q5}
+        CSV.generate(headers: true) do |csv|
+            csv << attributes
+
+            all.each do |survey|
+                csv << survey.attributes.values_at(*attributes)
+            end
+        end
+    end
 end
