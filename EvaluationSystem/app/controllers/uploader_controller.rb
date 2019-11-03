69ec9bc24fce46f7bcf0234c998c48df18dbb4c4
unknown <travisle22@gmail.com>
2019-11-03 14:09:43 -0800

    Change naming of files Reverted jquery (for now) Improved reading

---
+++ EvaluationSystem/app/controllers/uploader_controller.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+class UploaderController < ApplicationController
+  def new
+  end
+  def index
+  end
+  def create
+    uploaded_file = params[:file]
+    arr_of_arrs = CSV.parse(uploaded_file.read)
+    puts arr_of_arrs
+    redirect_to '/uploader/new'
+    
+  end
+  def extension_white_list
+    %w(csv)
+  end
+  def show
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+
+end
