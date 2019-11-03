ea76e7d8dc9ad74bd33a924628c9a2bacc4f4c42
unknown <travisle22@gmail.com>
2019-11-03 14:39:56 -0800

    Revert "Revert "Prop upload""
    This reverts commit a5e1d14b6c36d3f8f263ad061bc0007abf1ba574.

---
+++ EvaluationSystem/app/assets/javascripts/checker.js
@@ -0,0 +1,13 @@
+
+
+function fileisValid(event){
+    var files = event.target.files;
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function(){
+        alert(reader.result)
+    }
+    reader.readAsBinaryString(file)
+    return false
+}
+
