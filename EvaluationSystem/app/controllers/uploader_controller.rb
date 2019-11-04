021b608ab7edf158d100dae64a841ed937b14854
unknown <travisle22@gmail.com>
2019-11-03 18:51:20 -0800

    Added a very strict check to check CSVs

--- EvaluationSystem/app/controllers/uploader_controller.rb
+++ EvaluationSystem/app/controllers/uploader_controller.rb
@@ -12,16 +12,23 @@ class UploaderController < ApplicationController
     if theFrom == 'classes'
       
     elsif theFrom == 'roster'
-      CSV.parse(uploaded_file.read, headers: true) do |row|
-        @section = Section.new()
-        @section.class_num = row['Class Nbr']
-        @section.subject = row['Subject']
-        @section.catalog = row['Catalog']
-        @section.title = row['Title']
-        @section.enrolled = row['Enrollment after removal of drops']
-        @section.user_ID = 1
-        if !@section.save
-          puts 'error'
+      k = CSV.read(uploaded_file.path)[0]
+      if !check_header(k)
+        puts "Not Right"
+      else
+        CSV.foreach(uploaded_file.path, headers: true) do |row|
+          @section = Section.new()
+          @section.class_num = row['Class Nbr']
+          @section.subject = row['Subject']
+          @section.catalog = row['Catalog']
+          @section.title = row['Title']
+          @section.enrolled = row['Enrollment after removal of drops']
+          @section.user_ID = 1
+          if !Section.exists?(class_num: @section.class_num)
+            if !@section.save
+              puts 'error'
+            end
+          end
         end
       end
     end
@@ -39,5 +46,13 @@ class UploaderController < ApplicationController
   def destroy
 
   end
-
+end
+def check_header(x)
+  t = false
+  if x.include? 'Class Nbr' and x.include? 'Subject' and 
+      x.include? 'Catalog' and x.include? 'Title' and
+      x.include? 'Enrollment after removal of drops'
+    t = true
+  end
+  t
 end
