6468a4fe3efb0b170796de012d42d8381c4660dc
Emily Zhang <emily@Emilys-MacBook-Pro.local>
2019-11-17 13:31:33 -0800

    Reset back to before this final pres meeting.

--- EvaluationSystem/app/models/section.rb
+++ EvaluationSystem/app/models/section.rb
@@ -1,16 +1,27 @@
 class Section < ApplicationRecord
   #has_many :surveys, dependent: :destroy
-
-    def self.to_csv
-        @relevantSurveys = Survey.where(class_num: self.class_num)
-        #write method to print out surveys for each section
+    def to_csv
         attributes = %w{Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20}
-
         CSV.generate(headers: true) do |csv|
             csv << attributes
+            x = Survey.where(class_num: self.class_num )
+            x.all.each do |survey2|
+                csv << survey2.attributes.values_at(*attributes)
+            end
+        end
+    end
 
+    def self.to_csv
+        #@relevantSurveys = Survey.where(class_num: self.class_num)
+        #write method to print out surveys for each section
+        attributes = %w{Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20}
+        CSV.generate(headers: true) do |csv|
+            csv << attributes
             all.each do |survey|
-                csv << survey.attributes.values_at(*attributes)
+                x = Survey.where(class_num: survey.class_num )
+                x.all.each do |survey2|
+                    csv << survey2.attributes.values_at(*attributes)
+                end
             end
         end
     end
