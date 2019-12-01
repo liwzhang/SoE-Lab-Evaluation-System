76808f8146513d7f166eebeb2cd5a83199a76625
unknown <travisle22@gmail.com>
2019-12-01 15:45:44 -0800

    Added Comments for models

--- EvaluationSystem/app/models/application_record.rb
+++ EvaluationSystem/app/models/application_record.rb
@@ -1,3 +1,4 @@
+# This class is used to help with model management for a general purpose.
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- EvaluationSystem/app/models/question.rb
+++ EvaluationSystem/app/models/question.rb
@@ -1,3 +1,5 @@
+# This class is used to help with model management for question.
+# It handles data that is sent to the question model. So far no methods.
 class Question < ApplicationRecord
   # belongs_to :survey
 end
