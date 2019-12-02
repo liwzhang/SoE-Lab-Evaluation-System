e5e67b5a4fe55265cd8c7df69f9d238ec885bcea
Emily Zhang <emily@Emilys-MacBook-Pro.local>
2019-12-01 18:38:58 -0800

    Added sendgrid to heroku.

--- EvaluationSystem/config/environments/production.rb
+++ EvaluationSystem/config/environments/production.rb
@@ -69,14 +69,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://scu-evaluation-system.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "scu-evaluation-system.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "https://scu-evaluation-system.herokuapp.com",
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
+    address: "smtp.sendgrid.net",
+    port: 465,
+    domain: "scu-evaluation-system.herokuapp.com",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true
   }
