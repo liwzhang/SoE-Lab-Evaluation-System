47545cb92bdd08bdf2cd213514d41cfb58aebaad
Emily Zhang <zhang11liwen@gmail.com>
2019-12-01 16:45:54 -0800

    Update survey.rb

--- EvaluationSystem/app/models/survey.rb
+++ EvaluationSystem/app/models/survey.rb
@@ -4,7 +4,7 @@ class Survey < ApplicationRecord
 #   belongs_to :section
 #   has_many :questions
 
-    # Gets all the values in a survey table, and puts them in a CSV file which will be outputed.
+    # Gets all the values in a survey table, and puts them in a CSV file which will be outputed
     # Implicit parameters: ALL Survey objects
     def self.to_csv
         attributes = %w{Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20}
